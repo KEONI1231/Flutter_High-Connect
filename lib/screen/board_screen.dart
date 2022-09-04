35331ad715d715dbaa3c582ff61f244fe6a64583
JungHocheol <ghcjf2983@gmail.com>
2022-09-05 02:42:41 +0900

    middle code

--- lib/screen/board_screen.dart
+++ lib/screen/board_screen.dart
@@ -1,47 +1,57 @@
 import 'package:flutter/material.dart';
+import '../constant/color.dart';
 
 class BoardScreen extends StatelessWidget {
   const BoardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
+    return SafeArea(
+        child: Scaffold(
+      body: Column(
         children: [
           Homestyle('내가 쓴 글'),
           SizedBox(
-            height: 3.0,
+            height: 5.0,
           ),
           Homestyle('댓글 단 글'),
           SizedBox(
-            height: 3.0,
+            height: 5.0,
           ),
           Homestyle('스크랩'),
           SizedBox(
-            height: 3.0,
+            height: 5.0,
           ),
           Homestyle('HOT 게시판'),
           SizedBox(
-            height: 3.0,
+            height: 5.0,
           ),
           Homestyle('BEST 게시판'),
         ],
       ),
-    );
+    ));
   }
 }
 
 Widget Homestyle(String BoardName) {
   return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      //border: Border.all(width: 2, color: PRIMARY_COLOR),
+      borderRadius: BorderRadius.circular(16),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 100,
+            offset: Offset(0, 10))
+      ],
+    ),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Text(
-          BoardName,
-          style: TextStyle(
-            fontWeight: FontWeight.w900,
-            fontSize: 20
-          ),
+          BoardName, style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
         ),
       ],
     ),
