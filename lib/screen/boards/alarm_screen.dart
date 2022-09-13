756134169c5eb3a200bbfb46434f02066179cd45
yangayoung <ayoung9902@gmail.com>
2022-09-14 05:28:26 +0900

    Update alarm_screen.dart

--- lib/screen/boards/alarm_screen.dart
+++ lib/screen/boards/alarm_screen.dart
@@ -8,69 +8,37 @@ class alarmscreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Container(
-            child: Row(
-              children: [
-                IconButton(
-                    iconSize: 40.0,
-                    onPressed: () {
-                      showCupertinoDialog(
-                        context: context,
-                        barrierDismissible: true,
-                        builder: (BuildContext context) {
-                          return Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Container(
-                              color: Colors.lightBlue.shade50,
-                              height: 500.0,
-                            ),
-                          );
-                        },
-                      );
-                    },
-                    icon: Icon(
-                      Icons.lightbulb,
-                      color: Colors.yellow,
-                    )),
-                Text(
-                  '알림',
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text('    '),
-                IconButton(
-                    iconSize: 40.0,
-                    onPressed: () {
-                      showCupertinoDialog(
-                        context: context,
-                        barrierDismissible: true,
-                        builder: (BuildContext context) {
-                          return Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Container(
-                              color: Colors.lightBlue.shade50,
-                              height: 500.0,
-                            ),
-                          );
-                        },
-                      );
-                    },
-                    icon: Icon(
-                      Icons.messenger,
-                      color: Colors.red,
-                    )),
-                Text(
-                  '쪽지함',
-                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-              ],
+      body: Container(
+        child: Row(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: Text('   알림',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 24,
+                      color: CupertinoColors.inactiveGray)),
             ),
-          ),
-        ));
+            SizedBox(
+              height: 30,
+            ),
+            Text('    '),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                '쪽지함',
+                style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 24,
+                    color: CupertinoColors.inactiveGray),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
