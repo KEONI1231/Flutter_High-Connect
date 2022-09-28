08ff0b6d2f201bb1ed69258d7dd58984382463eb
keoni <kimkeonhwi991231@gmail.com>
2022-09-28 19:14:25 +0900

    깃 커밋 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,21 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:per_pro/screen/intro_screen.dart';
 
+//git test
+// git test 입니다
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
 
   runApp(
+
+
     MaterialApp(
       home: MyApp(), // 시작 부분.
     ),
+
+
+
+    
   );
 }
