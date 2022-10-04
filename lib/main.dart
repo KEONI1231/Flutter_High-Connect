3a6cfd02f860862738b8b9a3b24b562293f0afe4
cho0724 <dyddnayrma@naver.com>
2022-10-04 20:23:56 +0900

    pull위한 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:per_pro/screen/intro_screen.dart';
+import 'package:provider/provider.dart';
+
 
 //git test
 // git test 입니다
@@ -8,9 +10,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(
-    MaterialApp(
-      home: MyApp(), // 시작 부분.
-    ),
+  runApp(MaterialApp(
+        home: MyApp(), // 시작 부분.
+      ),
   );
 }
